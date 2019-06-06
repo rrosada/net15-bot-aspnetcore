78ea2e2286135cae50ec2a1ecac5ce416cd592b3
rrosada <rodrigocarlosrosada@yahoo.com.br>
2019-06-05 22:47:02 -0300

    commit

--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -17,23 +17,17 @@ namespace SimpleBotCore.Logic
             //var collection = db.GetCollection<BsonDocument>("col01");
 
             //var doc = new BsonDocument();
-            
+
             var doc = new BsonDocument()
             {
-                { "count", 0 },
                 { "campo1", 1 },
                 { "campo2", 2 },
-                { "campo3", new BsonDocument
-                            {
-                                { "A", 1 }
-                            }                
+                { "campo3",
+                    new BsonDocument { { "A", 1 } }
+                },
+                { "count", 0 }
             };
             
-            //var collection = db.GetCollection<BsonDocument>("col01");            
-            //r_q4b3swn2j8
-            //var filter = new SimpleMessage("r_q4b3swn2j8", "", "");
-            //var result = collection.Find(filter.Id);
-
             var collection = db.GetCollection<BsonDocument>("col01");
             
             var filter = new BsonDocument() {
